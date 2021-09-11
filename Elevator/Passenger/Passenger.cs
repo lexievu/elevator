9bb21217e5b3c5e4eca30cf80e18ed97a94375e6
Thien Huong Vu <huong.lexie.vu@gmail.com>
2021-09-11 15:58:00 +0100

    Passenger class created

---
+++ Elevator/Passenger/Passenger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PassengerNS
+{
+    public class Passenger
+    {
+        public readonly int id;
+        public int atFloor;
+        public int goingToFloor;
+        public int startWaitingAt;
+
+        public Passenger (int personID, int atFloor, int goingToFloor, int time)
+        {
+            this.id = personID;
+            this.atFloor = atFloor;
+            this.goingToFloor = goingToFloor;
+            this.startWaitingAt = time; 
+        }
+    }
+}
---
+++ Elevator/PassengerTests/PassengerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PassengerNS; 
+
+namespace PassengerTestsNS
+{
+    [TestClass]
+    public class PassengerTests
+    {
+        [TestMethod]
+        public void createPassenger()
+        {
+            Passenger pas = new Passenger(2, 5, 1, 4);
+
+            Assert.AreEqual(2, pas.id);
+            Assert.AreEqual(5, pas.atFloor);
+            Assert.AreEqual(1, pas.goingToFloor);
+            Assert.AreEqual(4, pas.startWaitingAt);
+        }
+    }
+}
