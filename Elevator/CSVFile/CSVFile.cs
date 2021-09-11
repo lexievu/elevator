dd30e9124ba9432af1dc4a4cdc5bb30be8dba6db
Thien Huong Vu <huong.lexie.vu@gmail.com>
2021-09-11 23:08:05 +0100

    Read from data.csv file to get a list of passengers - passed unit test

---
+++ Elevator/CSVFile/CSVFile.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using PassengerNS;
+using System.Collections.Generic;
+
+namespace CSVFileNS
+{
+    public class CSVFile
+    {
+        public static List<Passenger> readPassengersCSV()
+        {
+            var passengers = new List<Passenger>(); 
+            var path = @"/Users/thienhuongvu/elevator/data.csv"; // Person ID,At Floor,Going to Floor,Time
+            using (TextFieldParser csvParser = new TextFieldParser(path))
+            {
+                csvParser.CommentTokens = new string[] { "#" };
+                csvParser.SetDelimiters(new string[] { "," });
+                csvParser.HasFieldsEnclosedInQuotes = true;
+
+                // Skip the row with the column names
+                csvParser.ReadLine();
+
+                while (!csvParser.EndOfData)
+                {
+                    // Read current line fields, pointer moves to the next line.
+                    string[] fields = csvParser.ReadFields(); // Person ID,At Floor,Going to Floor,Time
+                    int personID = Int32.Parse(fields[0]);
+                    int atFloor = Int32.Parse(fields[1]);
+                    int goingToFloor = Int32.Parse(fields[2]);
+                    int time = Int32.Parse(fields[3]);
+                    passengers.Add(new Passenger(personID, atFloor, goingToFloor, time));
+                    // Console.WriteLine(personID.ToString() + " " + atFloor.ToString() + " " + goingToFloor.ToString() + " " + time.ToString());
+                }
+            }
+
+            return passengers;
+        }
+    }
+}
---
+++ Elevator/CSVFileTests/CSVFileTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSVFileNS;
+using System.Collections.Generic;
+using PassengerNS;
+
+namespace CSVFileTests
+{
+    [TestClass]
+    public class CSVUnitTests
+    {
+        [TestMethod]
+        public void ReadCSV()
+        {
+            List<Passenger> passengers = CSVFile.readPassengersCSV();
+
+            Assert.AreEqual(3, passengers[2].id);
+            Assert.AreEqual(1, passengers[12].atFloor);
+            Assert.AreEqual(259, passengers[23].startWaitingAt);
+            Assert.AreEqual(6, passengers[49].goingToFloor);
+        }
+    }
+}
